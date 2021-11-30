8c53afac34d917cbf36fdae4a03712af4db991d4
Bastien <bastien.nicoud@jobtrek.ch>
2021-11-30 08:29:38 +0100

    Add bell curve random number generator, centred to 4.25

--- js/app_4/random.js
+++ js/app_4/random.js
@@ -2,9 +2,15 @@ import { AppControl } from './control'
 
 export class Random extends AppControl {
   click () {
+    this.randomBell()
     Object.keys(this.calculator.grades).forEach(k => {
-      this.calculator.grades[k].setValue(Math.floor(Math.random() * (6 - 1 + 1) + 1))
+      this.calculator.grades[k].setValue(this.randomBell())
     })
     this.calculator.calculateAverages()
+  }
+
+  randomBell () {
+    const r = Math.round(((((Math.random() + Math.random()) / 2) * 6 + 1) + 0.75) / 0.5) * 0.5
+    return r > 6 ? 6 : r
   }
 }
